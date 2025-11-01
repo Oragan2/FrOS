62e832b6c8af7a23c87648d06dc28c37f16dbd2e
Oragan <antoine.pigny.2009@gmail.com>
2025-11-01 22:44:36 +0100

    Fixed : strcut returning a local stack variable and causing an error

--- string.c
+++ string.c
@@ -39,24 +39,20 @@ char *strconcat(char *dest, const char *src) {
     return original_dest;
 }
 
-char *strcut(const char *src, size_t n, int part) {
+void *strcut(const char *src, size_t n, int part, char *result) {
     size_t rlen = strlength(src);
     if (part != 1 && part != 2) return NULL;
     if (part == 1) {
-        char result[n + 1];
         for (size_t i = 0; i < n && src[i]; ++i) {
             result[i] = src[i];
         }
         result[n] = '\0';
-        return result;
     }
     else {
-        char result[rlen - n + 1];
         for (size_t i = n; i < rlen && src[i]; ++i) {
             result[i - n] = src[i];
         }
         result[rlen - n] = '\0';
-        return result;
     }
 }
 
