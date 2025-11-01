8f8f0d1ca6456347f6def90d265caa03c215ffda
Oragan <antoine.pigny.2009@gmail.com>
2025-11-01 22:46:09 +0100

    Fixed & Added : fixed the echo command and added a help command

--- cmd.c
+++ cmd.c
@@ -1,26 +1,28 @@
 #include "cmd.h"
 #include "string.h"
 #include "screen.h"
+#define NBCMD 3
 
 // Commands
 
-const char cmd_list[2][6] = {
+const char cmd_list[NBCMD][6] = {
     "cls\0",
-    "echo\0"
+    "echo\0",
+    "help\0"
 };
 
 // End Commands
 
 void cmdCheck(const char *s) {
-    char* cmd = NULL;
+    char cmd[20];
+    cmd[0] = '\0';
     int cmdF = 0;
     size_t pos = strpos(s, 32); // ASCII space is 32
     if (pos != -1) {
-        cmd = strcut(s, pos, 1);
-        print(cmd);
+        strcut(s, pos-1, 1, cmd);
     }
-    for (int i = 0; i < 2; ++i) {
-        if (cmd == NULL) 
+    for (int i = 0; i < NBCMD; ++i) {
+        if (cmd[0] == '\0')
             cmdF = strcmp(s, cmd_list[i]);
         else {
             cmdF = strcmp(cmd, cmd_list[i]);
@@ -31,8 +33,14 @@ void cmdCheck(const char *s) {
                     clear_screen();
                     return;
                 case 1: // echo
-                    print("Echo: ");
-                    print(s + 5);
+                    print(s + 5); // 5 is length of "echo "
+                    newLine();
+                    return;
+                case 2: // help
+                    print("Available commands:\n");
+                    print(" - cls: Clear the screen\n");
+                    print(" - echo [text]: Print text to the screen\n");
+                    print(" - help: Show this help message\n");
                     return;
                 default:
                     return;
