bcb6c38603e295a0c5ddee444f68e7b745c715b3
oragan <oragan@Linux.myguest.virtualbox.org>
2026-04-03 09:57:57 +0200

    Added the support for the files in the next commit

--- kernel.c
+++ kernel.c
@@ -2,11 +2,24 @@
 #include "io.h"
 #include "fs.h"
 #include <stdint.h>
+#include "pci.h"
+#include "idt.h"
+#include "pic.h"
+#include "pit.h"
 
 #define FS_START_LBA 9 
 #define FRFS_MAGIC "FRFS"
 
+extern void irq0_stub();
+
 void kernel_main(void) {
+    pic_remap();
+    idt_init();
+
+    idt_set_gate(32, (uint32_t)irq0_stub, 0x08, 0x8E); // IRQ0 = interrupt 32
+    
+    pit_init();
+
     clear_screen();
     if (initFRFS(&filesys)) {
 	setColor(0x04);
@@ -17,6 +30,7 @@ void kernel_main(void) {
     print("Hello, World from Kernel!\n");
     print("Started listening for keyboard input...\n");
     print("");
+    pci_scan();
     while (1) {
         keyboard_handler();
     }
