26bfe8c373eab9cde0990cb2b7dd82223df6986c
Oragan <antoine.pigny.2009@gmail.com>
2026-06-22 01:03:21 +0200

    Added 32gb paging

--- mem.c
+++ mem.c
@@ -1,8 +1,11 @@
 #include "mem.h"
 #include <stdint.h>
 
-#define KERNEL_VIRTUAL_SPACE 0xFFFFFFF
-#define VIRT_TO_PHYS(addr) ((uint64_t)(addr) & KERNEL_VIRTUAL_SPACE)
+#define KERNEL_VIRTUAL_SPACE 0xFFFFFFF800000000
+#define TO_PHYS(addr) ((uint64_t)(addr) & 0xFFFFFFF)
+
+#define HDDM_PML4_ADDR 256
+#define MAX_GB 32
 
 //bump allocator
 static unsigned char heap[0x100000];
@@ -10,9 +13,9 @@ static unsigned int heap_ptr = 0;
 unsigned char phys_bitmap[MAX_FRAMES/8];
 
 //MMU paging
-uint64_t pml4[512] __attribute__((aligned(4096)));
-uint64_t pdpt[512] __attribute__((aligned(4096)));
-uint64_t pd[512] __attribute__((aligned(4096)));
+uint64_t hddm_pml4[512] __attribute__((aligned(4096)));
+uint64_t hddm_pdpt[512] __attribute__((aligned(4096)));
+uint64_t hddm_pds[MAX_GB][512] __attribute__((aligned(4096)));
 
 void memcpy(void * dest, void * from, unsigned int size) {
 	for (unsigned int i = 0; i < size; ++i) {
@@ -30,9 +33,9 @@ int memcmp(void * c1, void * c2, unsigned int size) {
 }
 
 int memcheck(void * address, unsigned int size) {
-	for (unsigned int i = 0; i < size; ++i) {
+	for (unsigned int i = 0; i < size; ++i) 
 		if (((uint8_t*)address)[i] != 0) return 1;
-	}
+
 	return 0;
 }
 
@@ -55,22 +58,28 @@ void memset(void* dest, char val, unsigned int size) {
 }
 
 void pagingSetup() {
-	memset(pml4, 0, 4096);
-	memset(pdpt, 0, 4096);
-	memset(pd, 0, 4096);
+	memset(hddm_pdpt, 0, 4096);
+	memset(hddm_pds, 0, 4096);
 
-	pml4[0] = VIRT_TO_PHYS(pdpt) | 0x3;
+	hddm_pml4[0] = TO_PHYS(hddm_pdpt) | 0x3;
+	
+	unsigned int current_addr = 0;
 
-	pdpt[0] = VIRT_TO_PHYS(pd) | 0x3;
+	for (int gb = 0; gb < MAX_GB; ++gb) {
+		hddm_pdpt[gb] = TO_PHYS(&hddm_pds[gb]) | 0x3; // writable and accesible
 
-	pd[0] = 0x000000 | 0x83;
+		for (int i = 0; i < 512; ++i) {
+			hddm_pds[gb][i] = current_addr | 0x83;
+			current_addr += 2*(1024*1024);
+		}
+	}
 
 	// load register
 	__asm__ volatile (".intel_syntax noprefix\n\t"
 			"mov cr3, %0\n\t"
 			".att_syntax prefix\n\t"
 			: 
-			: "r"(VIRT_TO_PHYS(pml4))
+			: "r"(TO_PHYS(hddm_pml4))
 			: "memory"
 	);
 }
